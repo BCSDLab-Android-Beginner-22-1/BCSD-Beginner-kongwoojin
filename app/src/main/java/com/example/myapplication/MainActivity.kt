222429bbda17853bfedb3814d583dc7d772fc9f4
kongwoojin <kongwoojin03@gmail.com>
2022-05-10 22:02:42 +0900

    6주차 과제

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val dataList = mutableListOf<Names>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val addItemText: EditText = findViewById(R.id.add_item_text)
+        val addItemButton: Button = findViewById(R.id.add_item_button)
+
+        val adapter = NameAdapter()
+        adapter.dataList = dataList
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        addItemButton.setOnClickListener {
+            dataList.add(Names(addItemText.text.toString()))
+            addItemText.setText("")
+            adapter.notifyDataSetChanged()
+        }
+
+        adapter.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.dialog_delete_title))
+                .setMessage(getString(R.string.dialog_delete_message))
+                .setPositiveButton(
+                    getString(R.string.dialog_delete_confirm)
+                ) { _, _ ->
+                    dataList.removeAt(it)
+                    adapter.notifyDataSetChanged()
+                }
+                .setNegativeButton(
+                    getString(R.string.dialog_delete_dismiss)
+                ) { dialog, _ ->
+                    dialog.dismiss()
+                }
+            builder.show()
+        }
+
+        adapter.setOnLongClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = this.layoutInflater;
+
+            val rootView = inflater.inflate(R.layout.dialog_edit, null)
+            builder.setTitle(getString(R.string.dialog_change_title))
+                .setView(rootView)
+                .setPositiveButton(
+                    getString(R.string.dialog_change_confirm)
+                ) { _, _ ->
+                    val changeEditText: EditText = rootView.findViewById(R.id.change_edit_text)
+                    dataList.set(it, Names(changeEditText.text.toString()))
+                    adapter.notifyDataSetChanged()
+                }
+                .setNegativeButton(
+                    getString(R.string.dialog_change_dismiss)
+                ) { dialog, _ ->
+                    dialog.dismiss()
+                }
+            builder.show()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Names.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+class Names(var name: String)
